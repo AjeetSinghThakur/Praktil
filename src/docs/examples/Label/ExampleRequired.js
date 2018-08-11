8ab3f0c7edb11582de1dd9ee0bb986dc4c54cb98
Ajeet Thakur <ajeetthakur@Ajeets-MacBook-Pro.local>
2018-08-11 18:31:37 +0300

    Modified files and controls to make it reusable.

---
+++ src/docs/examples/Label/ExampleRequired.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import Label from 'praktil/Label';
+
+/** Required label */
+export default function ExampleRequired() {
+  return <Label htmlFor="test" label="test" required />
+}
---
+++ src/docs/examples/ProgressBar/Example10Percent.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ProgressBar from 'praktil/ProgressBar';
+
+/** 10% progress */
+export default function Example10Percent() {
+  return <ProgressBar percent={10} width={150} />
+}
